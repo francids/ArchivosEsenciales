5ab8d494f3bfc0c0f38f2512fea129c3d7e8a2ce
francids <francisco.mc.ids@gmail.com>
2024-07-18 08:39:10 -0400

    Agregar archivos de proyecto.

---
+++ ArchivosEsenciales/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace ArchivosEsenciales.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ ArchivosEsenciales/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace ArchivosEsenciales.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
