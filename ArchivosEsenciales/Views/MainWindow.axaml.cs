33907a9bcb86e330754138e8f17dfbae78304cec
francids <francisco.mc.ids@gmail.com>
2024-07-19 11:41:47 -0400

    Configuración del logo Configuración inicial de estilos y recursos

--- ArchivosEsenciales/Views/MainWindow.axaml.cs
+++ ArchivosEsenciales/Views/MainWindow.axaml.cs
@@ -1,11 +1,24 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace ArchivosEsenciales.Views;
 
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+
+	private void Window_KeyDown(object? sender, KeyEventArgs e)
+	{
+		//if (e.Key == Key.Escape)
+		//{
+		//	Close();
+		//}
+		if (e.Key == Key.Q && e.KeyModifiers.HasFlag(KeyModifiers.Control))
+		{
+			Close();
+		}
+	}
 }
