d0177ee8d7a050b6525859912d087778e14b7e51
francids <francisco.mc.ids@gmail.com>
2024-08-25 19:39:46 -0400

    Implementación inicial de la selección de archivo tipo imagen

--- ArchivosEsenciales/Views/MainView.axaml.cs
+++ ArchivosEsenciales/Views/MainView.axaml.cs
@@ -12,16 +12,27 @@ public partial class MainView : UserControl
 		InitializeComponent();
 	}
 
-	private async void OnOpenFileDialogClick(object sender, RoutedEventArgs e)
+	private async void openPdfDialog(object sender, RoutedEventArgs e)
 	{
 		var topLevel = TopLevel.GetTopLevel(this);
-		
-		// Start async operation to open the dialog.
+
 		var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
 		{
-			Title = "Selecciona un archivo",
-			AllowMultiple = false,
+			Title = "Selecciona un archivo PDF",
+			AllowMultiple = true,
 			FileTypeFilter = new[] { FilePickerFileTypes.Pdf }
+		});
+	}
+
+	private async void openImageDialog(object sender, RoutedEventArgs e)
+	{
+		var topLevel = TopLevel.GetTopLevel(this);
+
+		var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+		{
+			Title = "Selecciona una imagen",
+			AllowMultiple = true,
+			FileTypeFilter = new[] { FilePickerFileTypes.ImageAll }
 		});
 	}
 }
