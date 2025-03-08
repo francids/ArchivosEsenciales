c5f25f9b078cbedf978230b9b91b125f174495de
francids <francisco.mc.ids@gmail.com>
2025-03-07 22:02:38 -0400

    Centrar la aparición de las ventanas

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 
+#if WINDOWS
+using Microsoft.Maui.LifecycleEvents;
+#endif
+
 namespace ArchivosEsenciales;
 
 public static class MauiProgram
@@ -30,6 +34,37 @@ public static class MauiProgram
                 fonts.AddFont("Archivo-Thin.ttf", "ArchivoThin");
                 fonts.AddFont("Archivo-ThinItalic.ttf", "ArchivoThinItalic");
             });
+
+#if WINDOWS
+        builder.ConfigureLifecycleEvents(events =>
+        {
+            events.AddWindows(windowsLifecycleBuilder =>
+            {
+                windowsLifecycleBuilder.OnWindowCreated(window =>
+                {
+                    window.Activate();
+                    window.ExtendsContentIntoTitleBar = false;
+
+                    var handle = WinRT.Interop.WindowNative.GetWindowHandle(window);
+                    var id = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(handle);
+                    var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(id);
+                    if (appWindow is null) return;
+
+                    var displayArea =
+                        Microsoft.UI.Windowing.DisplayArea.GetFromWindowId(
+                            id,
+                            Microsoft.UI.Windowing.DisplayAreaFallback.Nearest
+                        );
+                    if (displayArea is null) return;
+
+                    var centeredPosition = appWindow.Position;
+                    centeredPosition.X = (displayArea.WorkArea.Width - appWindow.Size.Width) / 2;
+                    centeredPosition.Y = (displayArea.WorkArea.Height - appWindow.Size.Height) / 2;
+                    appWindow.Move(centeredPosition);
+                });
+            });
+        });
+#endif
 
 #if DEBUG
         builder.Logging.AddDebug();
